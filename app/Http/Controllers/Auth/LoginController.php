e82038846467088578b19a2a42d4485b68f162b7
m0sirhy <m0sirhy@gmail.com>
2020-08-10 21:43:41 +0300

    rredirect user based on role

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -39,5 +40,14 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+
+    public function redirectTo(){
+        if(Auth::user()->hasRole('SuperAdmin')){
+            $this->redirectTo='/dashboard';
+            return  $this->redirectTo;
+
+        }
     }
 }
